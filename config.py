bea9af1f75079ceaf454b8d7b2ce2a147ba9dcb2
Ajay <ajaykshirsagar500@gmail.com>
2022-10-30 21:45:36 +0530

    project configuration

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+CSV_FILE_PATH = "enter path"
+
+PORT_NUMBER = 5656
+
+PICKLE_FILE_PATH = os.path.join("model_cereal", "cereal.pkl")
+
+JSON_FILE_PATH = os.path.join("model_cereal", "cereal_data.json")
